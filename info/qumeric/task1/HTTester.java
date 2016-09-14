b3abcb4b5573fdfc8f0333dbac47fdfdc3d4c3e1
Qumeric <qumidium@gmail.com>
2016-09-14 15:38:19 +0300

    Basic task1 (WIP)

---
+++ info/qumeric/task1/HTTester.java
@@ -0,0 +1,25 @@
+package info.qumeric.task1;
+
+/**
+ * @author Valery Cherepanov <qumeric@gmail.com>
+ * @version 1.0
+ */
+
+public class HTTester {
+    public static void main(String[] args) {
+        HashTable ht = new HashTable();
+        System.out.println(ht.size());
+        String key = "key";
+        String value = "value";
+        System.out.format("Ht contains key: %b\n", ht.contains(key));
+        ht.put(key, value);
+        System.out.format("Ht contains key after putting: %b\n", ht.contains(key));
+        System.out.format("Value of key after putting: %s\n", ht.get(key));
+        ht.remove(key);
+        System.out.format("Ht contains key after removing: %b\n", ht.contains(key));
+        System.out.format("Value of key after removing: %s\n", ht.get(key));
+
+    }
+}
+
+
